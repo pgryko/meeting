fc112cd22f6075ba72ad4f15b002e80b4632f18c
Piotr Gryko <Piotr.Gryko@gmail.com>
2016-03-16 21:19:25 +0000

    Trying to add CSS generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -12,12 +13,26 @@ module.exports = {
   plugins: process.env.NODE_ENV === 'production' ? [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
-  ] : [],
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+          template: 'src/static/html_templates/index.tpl.html',
+          path: 'build/client',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+  ] : [
+    new HtmlWebpackPlugin({
+          template: 'src/static/html_templates/index.tpl.html',
+          path: 'build/client',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+  ],
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
+      { test: /\.css$/, exclude: /node_modules/, loader: "style-loader!css-loader" }
     ]
   }
 }
