d4319c3d3cc6f0367bd6e57587d354d6d7b85a3a
Piotr Gryko <Piotr.Gryko@gmail.com>
2016-06-19 23:35:14 +0100

    Re-enabled authentication for dashboard

--- src/client/routes/Routes.js
+++ src/client/routes/Routes.js
@@ -52,7 +52,7 @@ export default (store) => {
         <Route path="/contacts" component={Contacts}/>
         <Route path="/login" component={Login} />
         {/*<Route path="dashboard" component={Dashboard} onEnter={requireAuth} /> */}
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/dashboard" component={Dashboard} onEnter={requireAuth} />
         <Route path="*" component={NoMatch}/>
       </Route>
     </Route>
